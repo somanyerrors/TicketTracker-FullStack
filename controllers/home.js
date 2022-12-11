61fb06b61e5a3e2b569d136fd8e2c456329ab401
ShiftingDitto <98179572+ShiftingDitto@users.noreply.github.com>
2022-12-11 09:40:36 -0500

    update 51:06

---
+++ controllers/home.js
@@ -0,0 +1,32 @@
+const TicketList = require('../models/ticketlist')
+
+module.exports = {
+    getIndex : async (req, res) => {
+        try {
+            const tickets = await
+            TicketList.find()
+            res.render("index.ejs", { TicketList: tickets });
+        } catch (err) {
+            if (err) return res.status(500).send(err);
+        }
+    },
+    createTicket: async (req, res) => {
+        const newTicket = new TicketList(
+            {
+                // Make sure field names match
+                subject: req.body.subject,
+                description: req.body.description,
+                severity: req.body.severity,
+                assignedTo: req.body.assignedTo,
+                status: req.body.status
+            });
+        try {
+            await newTicket.save();
+            console.log(newTicket)
+            res.redirect("/");
+        } catch (err) {
+            if (err) return res.status(500).send(err);
+            res.redirect("/");
+        }
+    }
+}
